81bcc61eed9d795c93e1a804666c2a07ac11aaa6
Yangyang <jerry_tianyang@sina.com>
2024-03-05 17:35:53 +0800

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student();
+        System.out.println("What is your name?");
+        Scanner scanner1 = new Scanner(System.in);
+        String name = scanner1.nextLine();
+        student.setStudentName(name);
+        System.out.println("What is your student ID?");
+        Scanner scanner2 = new Scanner(System.in);
+        String id = scanner2.nextLine();
+        student.setStudentId(id);
+        System.out.println("Please input your time:");
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int number = 0;
+        scanner.nextLine();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Please input your extra activity:");
+            String input = scanner.nextLine();
+            number = student.addExtraActivity(input);
+        }
+        System.out.println("You have " + number + " activities.");
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,83 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+
+    private String[] extraActivities;
+    public Student() {
+    }
+
+    public Student(String studentId, String studentName, String[] extraActivities) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.extraActivities = extraActivities;
+    }
+
+    /**
+     * 获取
+     * @return studentId
+     */
+    public String getStudentId() {
+        return studentId;
+    }
+
+    /**
+     * 设置
+     * @param studentId
+     */
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    /**
+     * 获取
+     * @return studentName
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * 设置
+     * @param studentName
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * 获取
+     * @return extraActivities
+     */
+    public String[] getExtraActivities() {
+        return extraActivities;
+    }
+
+    /**
+     * 设置
+     * @param extraActivities
+     */
+    public void setExtraActivities(String[] extraActivities) {
+        this.extraActivities = extraActivities;
+    }
+
+    public String toString() {
+        return "Student{studentId = " + studentId + ", studentName = " + studentName + ", extraActivities = " + extraActivities + "}";
+    }
+
+    //该方法获取活动总数，并允许用户添加额外活动
+    public int addExtraActivity(String extraActivity) {
+        if (extraActivities == null) {
+            extraActivities = new String[0];
+        }
+        int newLength = extraActivities.length + 1;
+        String[] newExtraActivities = new String[newLength];
+        for (int i = 0; i < extraActivities.length; i++) {
+            newExtraActivities[i] = extraActivities[i];
+        }
+        newExtraActivities[newLength - 1] = extraActivity;
+        extraActivities = newExtraActivities;
+        return newLength;
+    }
 }
